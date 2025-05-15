23710bdc8be4df05ca93b69ed2bc695148b3a328
Bisrat Ayalew <bisratayalew88@gmail.com>
2025-05-15 07:58:03 +0300

    Update Mkdir.c

--- Mkdir.c
+++ Mkdir.c
@@ -1,1 +1,24 @@
+#include<stdio.h>
+#include<sys/stat.h>
+#include<sys/types.h>
+#include<errno.h>
 
+int main()
+{
+  const char *name = "linux";
+  mode_t type = 0755:
+
+  int result = mkdir(name,type);
+  if(result==0)
+  {
+    printf("successful");
+  }
+  else
+  {
+    perror("failed");
+  }
+return 0;
+}
+
+
+ 
